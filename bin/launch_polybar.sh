fd3a5d8ca6d209c118406b1f1fab37814f4e6a26
Karim N. Gorjux <karimone@gmail.com>
2019-09-23 14:04:01 +1000

    Update i3 and polybar launcher

--- bin/launch_polybar.sh
+++ bin/launch_polybar.sh
@@ -6,13 +6,11 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main -c $HOME/.config/polybar/config.ini &
-polybar bottom -c $HOME/.config/polybar/config.ini
 
-# if type "xrandr"; then
-#  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#    MONITOR=$m polybar -c ~/.config/polybar/config --reload example &
-#  done
-#else
-#  polybar --reload example &
-#fi
+if type "xrandr"; then
+    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+        MONITOR=$m polybar bottom -c $HOME/.config/polybar/config.ini & MONITOR=$m polybar main -c $HOME/.config/polybar/config.ini &
+    done
+else
+    polybar main -c $HOME/.config/polybar/config.ini & polybar bottom -c $HOME/.config/polybar/config.ini
+fi
